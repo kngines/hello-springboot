2bf68167940e45d52c7c16bcb4e7b12fb26bb2b6
kngines <kngines@163.com>
2019-04-11 02:30:39 +0800

    learn springboot skills

---
+++ src/main/java/com/kngines/springboot/SBIndexApplication01.java
@@ -0,0 +1,12 @@
+package com.kngines.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBIndexApplication01 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SBIndexApplication01.class, args);
+	}
+}
